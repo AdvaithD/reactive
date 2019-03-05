b7516a5a3b7b8d026b7de24b440e19d5ba0608df
Advaith Doosa <advaithseo@gmail.com>
2019-03-05 12:48:15 -0800

    first mutation added

--- src/index.js
+++ src/index.js
@@ -6,18 +6,25 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
     feed: () => links,
   },
-  // 3
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
   }
+
 }
 
 
