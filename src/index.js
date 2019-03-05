a8b95fa146ab8b1ac5226cf65db2eb2d7790402a
Advaith Doosa <advaithseo@gmail.com>
2019-03-05 12:30:45 -0800

    added links example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga')
+// 1
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
+}
 // 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
-// 2
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
 
 // 3
 const server = new GraphQLServer({
